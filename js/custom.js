dbca67ce9a668495e4c11ea801b5f13d422ce198
jb <info@sunshinetechnologyprograms.com>
2025-06-04 11:40:24 -0400

    slow down carousel

--- js/custom.js
+++ js/custom.js
@@ -7,29 +7,6 @@ function getYear() {
 
 getYear();
 
-
-//  owl carousel script
-$(".owl-carousel").owlCarousel({
-    loop: true,
-    margin: 20,
-    nav: true,
-    navText: [],
-    autoplay: true,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        1000: {
-            items: 2
-        }
-    }
-});
-
-//    end owl carousel script 
-
-
-
 /** google_map js **/
 function myMap() {
     var mapProp = {
